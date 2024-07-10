cd704bf25e7f936bcdfb3ec60c5621e159251318
vitdevelop <balanici_vitalik@mail.ru>
2024-07-10 09:05:58 +0300

    Define docker compose redirect network packets

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,3 +1,79 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::Result;
+use tokio::io::{stdout, AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+use tokio::signal::unix::{signal, SignalKind};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    let addr = "0.0.0.0:8080".to_string();
+
+    echo(&addr);
+
+    // TODO: fix handling in docker container when need execute command after
+    match signal(SignalKind::terminate())?.recv().await {
+        _ => Ok(()),
+    }
+}
+
+#[allow(unused)]
+fn echo(addr: &String) {
+    let addr = addr.clone();
+    tokio::spawn(async move {
+        let listener = TcpListener::bind(&addr).await.unwrap();
+        println!("Listening on: {}", addr);
+
+        loop {
+            let (mut socket, _) = listener.accept().await.unwrap();
+            tokio::spawn(async move {
+                let mut buf = vec![0; 1024];
+
+                let n = socket
+                    .read(&mut buf)
+                    .await
+                    .expect("failed to read data from socket");
+
+                stdout()
+                    .write_all(&buf[0..n])
+                    .await
+                    .expect("failed to write data to stdout");
+
+                stdout().flush().await.expect("failed to flush to stdout");
+
+                socket
+                    .write_all(&buf[0..n])
+                    .await
+                    .expect("failed to write data to socket");
+
+                socket.flush().await.expect("failed to flush socket");
+            });
+        }
+    });
+}
+
+#[allow(unused)]
+fn stdout_print(addr: &String) {
+    let addr = addr.clone();
+    tokio::spawn(async move {
+        let listener = TcpListener::bind(&addr).await.unwrap();
+        println!("Listening on: {}", addr);
+
+        loop {
+            let (mut socket, _) = listener.accept().await.unwrap();
+            tokio::spawn(async move {
+                let mut buf = vec![0; 1024];
+
+                let n = socket
+                    .read(&mut buf)
+                    .await
+                    .expect("failed to read data from socket");
+
+                stdout()
+                    .write_all(&buf[0..n])
+                    .await
+                    .expect("failed to write data to stdout");
+
+                stdout().flush().await.expect("failed to flush to stdout");
+            });
+        }
+    });
 }
---
+++ docker_debug/config-iptables.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+#iptables -t nat -A PREROUTING -p tcp --dport 8888 -j REDIRECT --to 8080
+NAT_TABLE="iptables -t nat"
+REDIRECT_RULE="PREROUTING -p tcp --dport 8888 -j REDIRECT --to 8080"
+REDIRECT_CHECK="$NAT_TABLE -C $REDIRECT_RULE"
+REDIRECT_APPEND="$NAT_TABLE -A $REDIRECT_RULE"
+REDIRECT_DELETE="$NAT_TABLE -D $REDIRECT_RULE"
+
+SAFE_EXIT_COMMAND=$(cat <<EOF
+ echo "#!/bin/ash
+ $REDIRECT_DELETE
+ exit" > /bin/clean_rules && chmod +x /bin/clean_rules
+EOF
+)
+ECHO_CLEAN_RULES=$(cat <<EOF
+echo "
+ ============================
+ Before exit call clean_rules
+ ============================"
+EOF
+)
+
+COMMAND=$(cat <<EOF
+ apk add iptables socat &&
+ ($REDIRECT_CHECK || $REDIRECT_APPEND) &&
+ $SAFE_EXIT_COMMAND &&
+ $ECHO_CLEAN_RULES &&
+ /bin/ash
+EOF
+)
+
+/bin/ash -c "$COMMAND"
